05256f8024705974a7f50352d6c6cd3af6cbd74b
ProgrammingHeroes <programmingh@gmail.com>
2015-03-10 19:23:04 +0100

    El nombre de los contactos será el identificador. Eliminado el Id.

--- AndroidChat/src/com/blogspot/programmingheroes/db/Contact.java
+++ AndroidChat/src/com/blogspot/programmingheroes/db/Contact.java
@@ -5,6 +5,7 @@ package com.blogspot.programmingheroes.db;
 
 import com.googlecode.objectify.annotation.Entity;
 import com.googlecode.objectify.annotation.Id;
+import com.googlecode.objectify.annotation.Index;
 
 
 /**
@@ -22,21 +23,20 @@ public class Contact
 	 * Identifica de forma única a este contacto.
 	 */
 	@Id
-	public Long id;
-	
-	/**
-	 * Nombre único del contacto.
-	 */
+	@Index
 	public String name;
 	
 	/**
 	 * Identificador que permitirá el envío de GCM a este contacto.
+	 * Debe de ser único puesto que se hay más de uno indicaría que una misma
+	 * persona tiene dos nombres distintos.
 	 */
+	@Index
 	public String regId;
 	
 	
 	/**
-	 * Crea un contacto vacío, sin id, sin nombre y sin regId
+	 * Crea un contacto vacío, sin nombre y sin regId.
 	 */
 	public Contact()
 	{	
@@ -45,7 +45,7 @@ public class Contact
 	/**
 	 * Crea un contacto a partir de un nombre y un regId para GCM.
 	 * 
-	 * @param name Nombre único del contacto.
+	 * @param name Nombre del contacto.
 	 * @param regId Id para GCM.
 	 */
 	public Contact(String name, String regId)
