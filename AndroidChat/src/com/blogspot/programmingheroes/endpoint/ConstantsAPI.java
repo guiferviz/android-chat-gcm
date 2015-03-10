788a1006c9abad6b6ea5d1a2739d0d79c3218b65
ProgrammingHeroes <programmingh@gmail.com>
2015-03-10 20:02:56 +0100

    Constantes de la API recopiladas en ConstantsAPI.

--- AndroidChat/src/com/blogspot/programmingheroes/endpoint/ConstantsAPI.java
+++ AndroidChat/src/com/blogspot/programmingheroes/endpoint/ConstantsAPI.java
@@ -1,17 +1,55 @@
 
 
-package com.blogspot.programmingheroes;
+package com.blogspot.programmingheroes.endpoint;
 
 
 import com.google.api.server.spi.Constant;
 
 
 /**
+ * Información básica de la API.
  * Contiene los IDs y los scopes para los clientes a los que se les
  * permite utilizar la API.
  */
-public class Constants
+public class ConstantsAPI
 {
+
+	/**
+	 * Título de la API.
+	 */
+	static final String TITLE = "Chat";
+
+	/**
+	 * Nombre de la API.
+	 */
+	static final String NAME = "chat";
+	
+	/**
+	 * Versión de la API.
+	 */
+	static final String VERSION = "v1";
+	
+	/**
+	 * Descripción de la API.
+	 */
+	static final String DESCRIPTION = "API para una aplicación de chats.";
+	
+	/**
+	 * Dominio. Constante para @ApiNamespace.
+	 */
+	static final String NAMESPACE_OWNER_DOMAIN =
+			"programmingheroes.blogger.com";
+	
+	/**
+	 * Nombre. Constante para @ApiNamespace.
+	 */
+	static final String NAMESPACE_OWNER_NAME = "ProgrammingHeroes";
+	
+	/**
+	 * Namespace. Constante para @ApiNamespace.
+	 */
+	static final String NAMESPACE_PACKAGE_PATH =
+			"programmingheroes/chat";
 	
 	/**
 	 * Sustituye esto por tu Web Client ID.
@@ -29,8 +67,14 @@ public class Constants
 	 */
 	public static final String IOS_CLIENT_ID = "";
 	
+	/**
+	 * Android audience, igual al Web Client ID.
+	 */
 	public static final String ANDROID_AUDIENCE = WEB_CLIENT_ID;
 	
+	/**
+	 * Permite ser acceder al enpoint desde el explorador de APIs de Google.
+	 */
 	public static final String API_EXPLORER_CLIENT_ID =
 			Constant.API_EXPLORER_CLIENT_ID;
 	
