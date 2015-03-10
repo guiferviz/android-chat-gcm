302f10a2b3ab80c7ce050513b7bd568af83733c7
ProgrammingHeroes <programmingh@gmail.com>
2015-03-10 23:46:21 +0100

    Completando DAO

--- AndroidChat/src/com/blogspot/programmingheroes/db/ContactDAO.java
+++ AndroidChat/src/com/blogspot/programmingheroes/db/ContactDAO.java
@@ -2,11 +2,11 @@
 
 package com.blogspot.programmingheroes.db;
 
+
 import java.util.List;
 
-import com.blogspot.programmingheroes.aigames.entities.Profile;
-import com.blogspot.programmingheroes.aigames.util.OfyService;
-import com.google.api.server.spi.response.ConflictException;
+import android.R.integer;
+
 import com.googlecode.objectify.cmd.Query;
 
 
@@ -19,6 +19,21 @@ import com.googlecode.objectify.cmd.Query;
 public class ContactDAO
 {
 
+	/**
+	 * Guarda un contacto en el datastore.
+	 * 
+	 * @param contact Contacto a guardar.
+	 * 
+	 * @return Objeto Contact con los datos del usuario (el mismo objeto que
+	 * se le pasa por parámetro).
+	 */
+	public static Contact save(Contact contact)
+	{
+		OfyService.ofy().save().entity(contact).now();
+		
+		return contact;
+	}
+	
 	/**
 	 * Lee un usuario a partir de su nombre.
 	 * 
@@ -39,10 +54,47 @@ public class ContactDAO
 	 * 
 	 * @return Todos los contactos en una lista.
 	 */
-	public List<Contact> getAllContacts()
+	public static List<Contact> readAll()
 	{
 		Query<Contact> query = OfyService.ofy().load().type(Contact.class);
 		return query.list();
+	}
+	
+	/**
+	 * Lee un usuario a partir de su GCM ID.
+	 * 
+	 * @param regId Id de GCM por el cual buscar al usuario.
+	 * 
+	 * @return Objeto Contact con los datos del usuario.
+	 */
+	public static Contact readByRegID(String regId)
+	{
+		Contact contact = null;
+		List<Contact> contacts = OfyService.ofy().load().type(Contact.class)
+				.filter("regid =", regId).list();
+		int size = contacts.size();
+		
+		if (size > 1)
+		{
+			throw new Error("Duplicated regId.");
+		}
+		else if (size == 1)
+		{
+			contact = contacts.get(0);
+		}
+		
+		return contact;
+	}
+	
+	/**
+	 * Borra un contacto determinado.
+	 * 
+	 * @param contact Contacto a borrar.
+	 * 
+	 */
+	public static void delete(Contact contact)
+	{
+		OfyService.ofy().delete().type(Contact.class).id(contact.name).now();
 	}
 	
 	/**
