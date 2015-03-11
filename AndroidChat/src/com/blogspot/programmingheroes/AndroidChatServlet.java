3a1b7821f79ca78a0b85269fa407765d058acfd7
ProgrammingHeroes <programmingh@gmail.com>
2015-03-11 18:18:26 +0100

    Servlet de pruebas que envía mensajes a todos los contactos.

--- AndroidChat/src/com/blogspot/programmingheroes/AndroidChatServlet.java
+++ AndroidChat/src/com/blogspot/programmingheroes/AndroidChatServlet.java
@@ -4,17 +4,14 @@ package com.blogspot.programmingheroes;
 
 
 import java.io.IOException;
-import java.io.FileReader;
-import java.io.BufferedReader;
-import java.io.PrintWriter;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import com.google.android.gcm.server.Sender;
-import com.google.android.gcm.server.Message;
-import com.google.android.gcm.server.Result;
+import com.blogspot.programmingheroes.db.Contact;
+import com.blogspot.programmingheroes.endpoint.ContactMessage;
+import com.blogspot.programmingheroes.gcm.ContactMessageSender;
 
 
 @SuppressWarnings("serial")
@@ -28,54 +25,28 @@ public class AndroidChatServlet extends HttpServlet
 		resp.getWriter().println("Android Chat");
 		resp.getWriter().println("Enviando mensaje de prueba.");
 		
-
-
-		Result result = null;
-
-		//String share = req.getParameter("shareRegId");
-
-		// GCM RedgId of Android device to send push notification
-		String regId = "";
-		/*if (share != null && !share.isEmpty()) {
-			regId = req.getParameter("regId");
-			PrintWriter writer = new PrintWriter("GCMRegId.txt");
-			writer.println(regId);
-			writer.close();
-			req.setAttribute("pushStatus", "GCM RegId Received.");
-			//req.getRequestDispatcher("index.jsp")
-			//		.forward(req, resp);
-		}
-		else*/
+		
+		try
 		{
-			try
-			{
-				//BufferedReader br = new BufferedReader(new FileReader(
-				//		"GCMRegId.txt"));
-				//regId = br.readLine();
-				//br.close();
-				regId = "APA91bEziSGrotNJY4AgzMQsgL2uS2RrYadsRc7wIme4hXu29gQ8LK_4-xfiKQZLxSRkCt3LQsFHWTzQKWAYr4bxDKU_VLLncmCmMRVcCttr4O_QUVyNH4dhc4r69rGfauHK_eSdU3ZetvX1YrT4Xq84acHoJtMxpFFVesYcH8_xV_pWm9om7-oXK24KTGYojHdeIhbhJAci";
-				String userMessage = "pene";//req.getParameter("message");
-				System.out.println("regId: " + regId);
-				Sender sender = new Sender(Constants.GOOGLE_SERVER_KEY);
-				Message message = new Message.Builder()
-						.addData("message", userMessage).build();
-				result = sender.sendNoRetry(message, regId);
-				//req.setAttribute("pushStatus", result.toString());
-			}
-			catch (IOException e)
-			{
-				e.printStackTrace();
-				//req.setAttribute("pushStatus",
-				//		"RegId required: " + e.toString());
-			}
-			catch (Exception e)
-			{
-				e.printStackTrace();
-				//req.setAttribute("pushStatus", e.toString());
-			}
+			//"APA91bEziSGrotNJY4AgzMQsgL2uS2RrYadsRc7wIme4hXu29gQ8LK_4-xfiKQZLxSRkCt3LQsFHWTzQKWAYr4bxDKU_VLLncmCmMRVcCttr4O_QUVyNH4dhc4r69rGfauHK_eSdU3ZetvX1YrT4Xq84acHoJtMxpFFVesYcH8_xV_pWm9om7-oXK24KTGYojHdeIhbhJAci";
+			String senderName = req.getParameter("sender");
+			Contact sender = new Contact(senderName, "");
+			String message = req.getParameter("message");
+			ContactMessage msg = new ContactMessage(sender, message);
 			
-			//req.getRequestDispatcher("index.jsp")
-			//		.forward(req, resp);
+			resp.getWriter().println();
+			resp.getWriter().println("Sender name: " + senderName);
+			resp.getWriter().println("Sender message: " + message);
+			
+			ContactMessageSender.sendToAllContacts(msg);
+		}
+		catch (IOException e)
+		{
+			e.printStackTrace();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
 		}
 	}
 
